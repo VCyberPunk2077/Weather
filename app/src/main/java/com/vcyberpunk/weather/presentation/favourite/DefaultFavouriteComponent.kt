486b26a9fc2ec2a8fba1a5846f120001abb10943
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-05-07 17:37:05 +0300

    feat: add DI to components

--- app/src/main/java/com/vcyberpunk/weather/presentation/favourite/DefaultFavouriteComponent.kt
+++ app/src/main/java/com/vcyberpunk/weather/presentation/favourite/DefaultFavouriteComponent.kt
@@ -6,17 +6,19 @@ import com.arkivanov.mvikotlin.extensions.coroutines.labels
 import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
 import com.vcyberpunk.weather.domain.entity.City
 import com.vcyberpunk.weather.presentation.extensions.componentScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-class DefaultFavouriteComponent @Inject constructor(
+class DefaultFavouriteComponent @AssistedInject constructor(
     private val favouriteStoreFactory: FavouriteStoreFactory,
-    private val onCityItemClicked: (City) -> Unit,
-    private val onSearchClicked: () -> Unit,
-    private val onAddToFavouriteClicked: () -> Unit,
-    componentContext: ComponentContext
+    @Assisted("onCityItemClicked") private val onCityItemClicked: (City) -> Unit,
+    @Assisted("onSearchClicked") private val onSearchClicked: () -> Unit,
+    @Assisted("onAddToFavouriteClicked") private val onAddToFavouriteClicked: () -> Unit,
+    @Assisted("componentContext") componentContext: ComponentContext
 ) : FavouriteComponent, ComponentContext by componentContext {
 
     private val store = instanceKeeper.getStore {
@@ -56,5 +58,15 @@ class DefaultFavouriteComponent @Inject constructor(
 
     override fun onClickAddFavourite() {
         store.accept(FavouriteStore.Intent.ClickAddToFavourite)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            @Assisted("onCityItemClicked") onCityItemClicked: (City) -> Unit,
+            @Assisted("onSearchClicked") onSearchClicked: () -> Unit,
+            @Assisted("onAddToFavouriteClicked") onAddToFavouriteClicked: () -> Unit,
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DefaultFavouriteComponent
     }
 }
--- app/src/main/java/com/vcyberpunk/weather/presentation/search/DefaultSearchComponent.kt
+++ app/src/main/java/com/vcyberpunk/weather/presentation/search/DefaultSearchComponent.kt
@@ -6,18 +6,20 @@ import com.arkivanov.mvikotlin.extensions.coroutines.labels
 import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
 import com.vcyberpunk.weather.domain.entity.City
 import com.vcyberpunk.weather.presentation.extensions.componentScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-class DefaultSearchComponent @Inject constructor(
-    private val openReason: OpenReason,
+class DefaultSearchComponent @AssistedInject constructor(
     private val searchStoreFactory: SearchStoreFactory,
-    private val onBackClicked: () -> Unit,
-    private val onCityClicked: (City) -> Unit,
-    private val onSavedToFavouriteClicked: () -> Unit,
-    componentContext: ComponentContext
+    @Assisted("openReason") private val openReason: OpenReason,
+    @Assisted("onBackClicked") private val onBackClicked: () -> Unit,
+    @Assisted("onCityClicked") private val onCityClicked: (City) -> Unit,
+    @Assisted("onSavedToFavouriteClicked") private val onSavedToFavouriteClicked: () -> Unit,
+    @Assisted("componentContext") componentContext: ComponentContext
 ) : SearchComponent, ComponentContext by componentContext {
 
     private val store = instanceKeeper.getStore {
@@ -61,5 +63,16 @@ class DefaultSearchComponent @Inject constructor(
 
     override fun onClickCity(city: City) {
         store.accept(SearchStore.Intent.ClickCity(city))
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(
+            @Assisted("openReason") openReason: OpenReason,
+            @Assisted("onBackClicked") onBackClicked: () -> Unit,
+            @Assisted("onCityClicked") onCityClicked: (City) -> Unit,
+            @Assisted("onSavedToFavouriteClicked") onSavedToFavouriteClicked: () -> Unit,
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DefaultSearchComponent
     }
 }
