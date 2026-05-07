20059a4ac9c28c71421b43d905fddb0d03ec267e
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-05-07 17:18:19 +0300

    feat: add search component

--- app/src/main/java/com/vcyberpunk/weather/presentation/search/DefaultSearchComponent.kt
+++ app/src/main/java/com/vcyberpunk/weather/presentation/search/DefaultSearchComponent.kt
@@ -1,7 +1,65 @@
 package com.vcyberpunk.weather.presentation.search
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.vcyberpunk.weather.domain.entity.City
+import com.vcyberpunk.weather.presentation.extensions.componentScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DefaultSearchComponent(
+class DefaultSearchComponent @Inject constructor(
+    private val openReason: OpenReason,
+    private val searchStoreFactory: SearchStoreFactory,
+    private val onBackClicked: () -> Unit,
+    private val onCityClicked: (City) -> Unit,
+    private val onSavedToFavouriteClicked: () -> Unit,
     componentContext: ComponentContext
-) : SearchComponent, ComponentContext by componentContext
+) : SearchComponent, ComponentContext by componentContext {
+
+    private val store = instanceKeeper.getStore {
+        searchStoreFactory.create(openReason)
+    }
+    private val scope = componentScope()
+
+    init {
+        scope.launch {
+            store.labels.collect {
+                when (it) {
+                    SearchStore.Label.ClickBack -> {
+                        onBackClicked()
+                    }
+                    is SearchStore.Label.OpenForecast -> {
+                        onCityClicked(it.city)
+                    }
+                    SearchStore.Label.SavedToFavourite -> {
+                        onSavedToFavouriteClicked()
+                    }
+                }
+            }
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<SearchStore.State>
+        get() = store.stateFlow
+
+    override fun onChangeSearchQuery(query: String) {
+        store.accept(SearchStore.Intent.ChangeSearchQuery(query))
+    }
+
+    override fun onClickBack() {
+        store.accept(SearchStore.Intent.ClickBack)
+    }
+
+    override fun onClickSearch() {
+        store.accept(SearchStore.Intent.ClickSearch)
+    }
+
+    override fun onClickCity(city: City) {
+        store.accept(SearchStore.Intent.ClickCity(city))
+    }
+}
