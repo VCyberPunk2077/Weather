fe4e891bd93e434123a3227d25f33e50451dce8e
VCyberPunk2077 <semenstasyuk@gmail.com>
2026-05-10 03:25:52 +0300

    feat: complete details screen

--- app/src/main/java/com/vcyberpunk/weather/presentation/extensions/Utils.kt
+++ app/src/main/java/com/vcyberpunk/weather/presentation/extensions/Utils.kt
@@ -1,11 +1,14 @@
 package com.vcyberpunk.weather.presentation.extensions
 
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.essenty.lifecycle.doOnDestroy
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import java.util.Locale
 import kotlin.math.roundToInt
 
 fun ComponentContext.componentScope(): CoroutineScope = CoroutineScope(
@@ -15,3 +18,13 @@ fun ComponentContext.componentScope(): CoroutineScope = CoroutineScope(
 }
 
 fun Float.tempToFormattedString(): String = "${roundToInt()}°C"
+
+fun Calendar.formattedFullDay(): String {
+    val format = SimpleDateFormat("EEEE | d MMM y", Locale.getDefault())
+    return format.format(time)
+}
+
+fun Calendar.formattedShortDayOfWeek(): String {
+    val format = SimpleDateFormat("EEE", Locale.getDefault())
+    return format.format(time)
+}
